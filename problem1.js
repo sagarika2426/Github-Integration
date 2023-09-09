fc793f2faf5949a246aa595e7eb7603fa7d1813c
sagarika2426 <66350066+sagarika2426@users.noreply.github.com>
2023-09-09 18:15:28 +0000

    if else statement

---
+++ problem1.js
@@ -0,0 +1,11 @@
+var shakal = "adult";
+var age = 18;
+if(shakal == "adult") {
+	console.log('allowed', shakal);
+}
+	else if (age >= 18) {
+		console.log('allowed', shakal, age);		
+	}
+	else {
+		console.log("not allowed", shakal);
+	}
